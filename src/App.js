ce543682b8b928c2dc4662f6c7a5879798d3c9aa
misbah912 <125118731+misbah912@users.noreply.github.com>
2023-03-04 20:56:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+/* import About from './components/About'; */
+
+function App() {
+  const[mode, setMode] = useState('light');
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = "#042743";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
+  return (
+    <>
+      {/* <Navbar title="TextUtils" aboutText="About Us"/> */}
+      {/* <Navbar/> */}
+      <Navbar title="TextUtils" mode = {mode} toggleMode={toggleMode}/>
+      <div className="container my-3">
+        <TextForm heading="Enter the text to analyze below" mode = {mode}/>
+       {/*  <About/> */}
+        </div>
+    </>
+  );
+}
+
+export default App;
